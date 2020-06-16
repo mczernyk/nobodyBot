b82d020c5451f3e2d85605c43983e720363e09bc
mczernyk <mczernyk@gmail.com>
2020-06-16 18:43:06 -0400

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'iwsRyMcjl2L5Q6lsOqPGZUaeE',
+  consumer_secret:      '02HIn5HgrMWV75dFRG3f3sl6zV7z9KpQ90Ju23ZLFumF0pT987',
+  access_token:         '966737735506710529-6ZbSh1NR0slcllWkVVIYuDA9jsIUXsu',
+  access_token_secret:  'kTBzyQdXFmpipXuUXxfYEShJW1kqfdxClBFuvVKPDsAmr'
+}
