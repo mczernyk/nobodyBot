8da6d542c9ba7c8890c5e4ceb2743e0e2f3c7ebd
mczernyk <mczernyk@gmail.com>
2020-06-16 02:32:29 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var path = require('path');
+var Twit = require('twit');
+var config = require('./config.js');
+var jokes = require('./jokes.js');
+
+var T = new Twit(config);
+
+console.log('config', config)
+
+let current = 0
+function random_from_array(jokes){
+  let joke = jokes[current];
+  current ++
+  if (current === jokes.length){
+    current = 0
+  }
+  return joke
+}
+
+function upload_random_image(jokes){
+  console.log('Opening an image...');
+  var joke = random_from_array(jokes);
+  var joke_path = path.join( __dirname, '/images/' + joke.image)
+  var joke_text = joke.text
+  console.log('jokePAth', joke_path)
+  console.log('jokeTExt', joke_text)
+  var b64content = fs.readFileSync(joke_path, { encoding: 'base64' });
+
+  console.log('Uploading an image...');
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:');
+      console.log(err);
+    }
+    else{
+      console.log('Image uploaded!');
+      console.log('Now tweeting it...');
+
+      T.post('statuses/update', {
+        status: joke_text,
+        media_ids: new Array(data.media_id_string)
+      },
+        function(err, data, response) {
+          if (err){
+            console.log('ERROR:');
+            console.log(err);
+          }
+          else{
+            console.log('Posted an image!');
+          }
+        }
+      );
+    }
+  });
+}
+
+fs.readdir(__dirname + '/images', function(err, files) {
+  if (err){
+    console.log(err);
+  }
+  else{
+    var images = [];
+    jokes.forEach(function(f) {
+      images.push(f);
+    });
+
+    setInterval(function(){
+      upload_random_image(images);
+    }, 1000 * 60 * 60 * 4);
+  }
+});
---
+++ jokes.js
@@ -0,0 +1,240 @@
+var jokes = [
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop001.png"
+  },
+  {
+    text: "Why does the Norway navy have barcodes on the side of their ships? \r\n\r\nSo when they come back to port they can Scandanavian 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "001.png"
+  },
+  {
+    text: "Which ghost is the best dancer? \r\n\r\nThe Boogie Man 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "012.png"
+  },
+  {
+    text: "I told my doctor that I broke my arm in two places. \r\n\r\nHe told me to stop going to those places 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "002.png"
+  },
+  {
+    text: "Did you hear about the Italian chef that died? \r\n\r\nHe pasta away 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "003.png"
+  },
+  {
+    text: "Did you know alligators can grow up to 15 feet? \r\n\r\nBut most have four 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "004.png"
+  },
+  {
+    text: "How does NASA organize a party? \r\n\r\nThey planet 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "005.png"
+  },
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop002.png"
+  },
+  {
+    text: "If a child refuses to sleep during nap time, are they guilty of resisting a rest? 👴#dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "006.png"
+  },
+  {
+    text: "Whats Fast and the Furious 10 going to be called? \r\n\r\nFast 10: your seatbelts 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "007.png"
+  },
+  {
+    text: "What kind of pants do ghosts wear? \r\n\r\nBoo jeans 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "008.png"
+  },
+  {
+    text: "I have a fear of elevators... \r\n\r\nBut I've started taking steps to avoid it 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "014.png"
+  },
+  {
+    text: "What do you call a man with no nose or body? \r\n\r\nNobody nose! 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "015.png"
+  },
+  {
+    text: "What do snowmen wear on their heads? \r\n\r\nIce caps! 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "016.png"
+  },
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop003.png"
+  },
+  {
+    text: "What do you call a president with lots of laundry to do? \r\n\r\nWashing-ton 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "020.png"
+  },
+  {
+    text: "I used to be addicted to lunch meats? \r\n\r\nBut I quit cold turkey 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "021.png"
+  },
+  {
+    text: "What is a ghost's favorite dessert? \r\n\r\nI Scream 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "011.png"
+  },
+  {
+    text: "Did you hear about the restaurant on the moon \r\n\r\nGreat food, no atmosphere 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "022.png"
+  },
+  {
+    text: "I can tell just by looking when someone is lying \r\n\r\nI can also tell when they're standing 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "023.png"
+  },
+  {
+    text: "Did you hear about the fire at the circus? \r\n\r\nIt was in tents 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "024.png"
+  },
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop004.png"
+  },
+  {
+    text: "Why do crabs never give to charity? \r\n\r\nBecause they're shellfish 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "025.png"
+  },
+  {
+    text: "How do snowmen get around? \r\n\r\nThey ride an icicle! 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "017.png"
+  },
+  {
+    text: "What does a skeleton say before dinner? \r\n\r\nBone appetit 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "010.png"
+  },
+  {
+    text: "What's it called when you have too many aliens? \r\n\r\nExtra-Terrestrials 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "027.png"
+  },
+  {
+    text: "I named my dogs Rolex and Timex \r\n\r\nThey're Watch Dogs 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "028.png"
+  },
+  {
+    text: "What do you call a pig who does karate? \r\n\r\nA pork chop 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "029.png"
+  },
+  {
+    text: "#cryptocurrency prices may be unpredictable, but #dadwear from www.radadsofficial.com is always a rock solid buy - thats 'investment advice' that you can take to the bank. Oh yea, #weacceptbitcoin 👴 #raddadsofficial #bitcoin #btc #ethereum #weacceptcrypto #eth #giftsfordad #dad",
+    image: "Shop005.png"
+  },
+  {
+    text: "Why did the cell phone get glasses? \r\n\r\nIt lost its contacts 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "030.png"
+  },
+  {
+    text: "Why don't scientists trust atoms? \r\n\r\nBecause they make up everything 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "031.png"
+  },
+  {
+    text: "How did the barber win the race? \r\n\r\nHe took a short cut 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "032.png"
+  },
+  {
+    text: "My son's math teacher called him average... \r\n\r\nI think he's mean 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "033.png"
+  },
+  {
+    text: "What happens when frogs park illegally? \r\n\r\nThey get toad 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "034.png"
+  },
+  {
+    text: "What do mummies listen to on #halloween? \r\n\r\nWrap music 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "035.png"
+  },
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop006.png"
+  },
+  {
+    text: "What do you take at vampire school? \r\n\r\nBlood tests 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "036.png"
+  },
+  {
+    text: "How did the computer get a hangover? \r\n\r\nToo many screenshots 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "037.png"
+  },
+  {
+    text: "Trying to save money on heat this winter? \r\n\r\nJust stand in the corner, it's 90 degrees 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "038.png"
+  },
+  {
+    text: "What did the shy pebble wish for? \r\n\r\nTo be a little boulder 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "039.png"
+  },
+  {
+    text: "How did an octopus beat a shark in a fight? \r\n\r\nIt was well armed 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "026.png"
+  },
+  {
+    text: "What do you call a cow with no legs? \r\n\r\nGround beef 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "040.png"
+  },
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop007.png"
+  },
+  {
+    text: "Why do ghosts like to ride in elevators? \r\n\r\nIt raises their spirits 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "013.png"
+  },
+  {
+    text: "What's Forrest Gump's email password? \r\n\r\n1forrest1 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "041.png"
+  },
+  {
+    text: "What did the ocean say to the shore? \r\n\r\nNothing, it just waved 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "042.png"
+  },
+  {
+    text: "What's the easter bunny's favorite kind of music? \r\n\r\nHip hop 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "043.png"
+  },
+  {
+    text: "What do you do if you get injured playing peek-a-boo? \r\n\r\nGo to the ICU 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "044.png"
+  },
+  {
+    text: "Where do I get all my dadjokes? \r\n\r\nFrom a dad-abase 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "045.png"
+  },
+  {
+    text: "Head on over to the www.raddadsofficial.com webstore to browse the finest #dadwear on the web. Shirts, hats, accessories... you name it, we've got it 👴 #raddadsofficial #giftsfordad #fathersday #fathersdaygifts #dadhat #dadshirt #dadgear #dadstuff #fordads",
+    image: "Shop008.png"
+  },
+  {
+    text: "The first 'French' fries weren't cooked in France... \r\n\r\nThey were cooked in 'Greece' 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "046.png"
+  },
+  {
+    text: "What are the strongest days of the week? \r\n\r\nSaturday and Sunday, the rest are 'weak' days 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "047.png"
+  },
+  {
+    text: "What do you get when you cross a snowman with a vampire? \r\n\r\nFrostbite! 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "018.png"
+  },
+  {
+    text: "I had a dream that I was a muffler last night... \r\n\r\nI woke up exhausted 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "048.png"
+  },
+  {
+    text: "Who is Santa's favorite singer? \r\n\r\nElf-is Presley 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "019.png"
+  },
+  {
+    text: "A farmer friend spent his stimulus check on baby chickens. \r\n\r\nHe got his money for nothing and his chicks for free 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "049.png"
+  },
+  {
+    text: "Have you ever seen a math tree? \r\n\r\nIt's like a regular tree, but it has square roots 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "050.png"
+  },
+  {
+    text: "How do runners handle criticism? \r\n\r\nThey take it in stride 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "051.png"
+  },
+  {
+    text: "What do you call two ghosts that are in love? \r\n\r\nBoo'd Up 👴 #dadjoke #dadhumor #dadmemes #dadmeme #dadjokes #raddadsofficial",
+    image: "009.png"
+  },
+]
+
+module.exports = jokes
