bf22252a077ae673ce787d1ea26daea4f13a86a6
mczernyk <mczernyk@gmail.com>
2022-01-25 03:34:06 -0500

    config trougleshooting

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  consumer_key:         'Lb7NlWvd0lvV9OpHjQWcgAaMR',
+  consumer_secret:      'JLNKGS6J748owKHnzdWI5DlSNswJVNLzk245AfyyZsfTmvpCCx',
+  access_token:         '1451624767849738254-seW6rCcvsxyi2tyt2vNWDOA3QVP8VC',
+  access_token_secret:  'aqKI5qRjBTSXXc5RuHL7i0ygQCCmlLzf4MwxMKboEcttm'
+}
+
+// bearer token
+// AAAAAAAAAAAAAAAAAAAAAITFXgEAAAAAMxZlVnlkeD3Dpk1PzMY7Tgntkh8%3D4xAw7UAlJdzL5gGv6qpZ8x3DRJ88afEEntetA8VreUaTxMW1Yu
