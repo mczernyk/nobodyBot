4706552854e13a36ce1d98a0f0c95f4c6d7b2676
mczernyk <mczernyk@gmail.com>
2023-05-12 04:59:49 -0400

    rolled back, revised imgs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var Twit = require('twit');
 var config = require('./config.js');
 var cron = require('node-cron');
 var products = require('./products.js');
+var dads = require('./dads.js');
+
 
 
 // to deploy:
@@ -34,6 +36,22 @@ function random_from_array_product(products){
     console.log('hit error or dupe')
 
     random_from_array_product(products)
+  }
+}
+
+let usedDads = []
+function random_from_array(dads){
+  let dad = dads[Math.floor(Math.random() * dads.length)]
+
+  if (!usedDads.includes(dad.edition)){
+    usedDads.push(dad.edition)
+
+    return dad
+  } else {
+    if (usedDads.length === dads.length){
+      usedDads = []
+    }
+    random_from_array(dads)
   }
 }
 
@@ -92,6 +110,55 @@ function upload_random_image_product(products){
   });
 }
 
+const upload_random_image = (dads) => {
+  console.log('Opening an image...');
+
+  var newDad = random_from_array(dads);
+  var dad_path = path.join( __dirname, '/dads/' + newDad.image)
+  var dad_text = `${newDad.name} \r\n\r\n mint your Dad at https://www.dadbro.xyz`
+  var b64content = fs.readFileSync(dad_path, { encoding: 'base64' });
+
+  console.log('Uploading an image...');
+
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR uploading image:');
+      console.log(err);
+    }
+    else {
+      console.log("dad_text", dad_text)
+      console.log("dad_image", dad_path)
+      console.log('image uploaded!');
+      console.log('Now tweeting it...');
+
+      T.post('media/metadata.create', {
+        media_id: data.media_id_string,
+                alt_text: {
+                    text: newDad.name
+                }
+      }, (err, data, response) => {
+        console.log('tweeting')
+
+        T.post('statuses/update', {
+          status: dad_text,
+          media_ids: new Array(data.media_id_string)
+        }, function(err, data, response) {
+            if (err){
+              console.log('ERROR during posting.');
+              console.log(err);
+            }
+            else{
+              console.log('Posted an image!');
+              console.log("The tweet says: " + dad_text);
+            }
+          }
+        );
+      })
+    }
+  });
+}
+
+
 fs.readdir(__dirname + '/products', function(err, files) {
   if (err){
     console.log(err);
@@ -103,26 +170,12 @@ fs.readdir(__dirname + '/products', function(err, files) {
       productImages.push(f);
     });
 
-    // cron.schedule('00 01 * * *', () => {
-    //   upload_random_image_product(productImages);
-    // }, {
-    //   scheduled: true,
-    //   timezone: "America/New_York"
-    // });
-
     cron.schedule('00 11 * * *', () => {
       upload_random_image_product(productImages);
     }, {
       scheduled: true,
       timezone: "America/New_York"
     });
-
-    // cron.schedule('00 13 * * *', () => {
-    //   upload_random_image_product(productImages);
-    // }, {
-    //   scheduled: true,
-    //   timezone: "America/New_York"
-    // });
 
     cron.schedule('00 19 * * *', () => {
       upload_random_image_product(productImages);
@@ -132,3 +185,25 @@ fs.readdir(__dirname + '/products', function(err, files) {
     });
   }
 });
+
+fs.readdir(__dirname + '/dads', function(err, files) {
+  if (err){
+    console.log(err);
+  }
+  else{
+    var dadImages = [];
+
+    dads.forEach(function(f) {
+      dadImages.push(f);
+    });
+
+    cron.schedule('00 15 * * *', () => {
+      upload_random_image(dadImages)
+    }, {
+      scheduled: true,
+      timezone: "America/New_York"
+    });
+  }
+});
+
+
